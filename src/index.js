9ac51b703fb548508afe15c7e56734dfc8a55aa7
faulty <40147859+faultyx@users.noreply.github.com>
2024-09-17 15:00:09 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-process.title = "Autumn v2.0 beta";
+process.title = "Autumn beta";
+
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+const auth = require("./auth/secrets.json");
+const prefix = require("./auth/conf.json");
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.login(auth.token)
+.catch(console.error);
