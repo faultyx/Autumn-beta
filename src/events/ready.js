34ff6979b57dc375adfcc3d3b19b1dfc2f4cb7f6
faulty <40147859+faultyx@users.noreply.github.com>
2024-09-18 23:57:44 +0100

    Testing code 09/18/2024

---
+++ src/events/ready.js
@@ -0,0 +1,10 @@
+// Testing with Sample Discord.js Ready Event
+const { Events } = require('discord.js');
+
+module.exports = {
+	name: Events.ClientReady,
+	once: true,
+	execute(client) {
+		console.log('Ready!');
+	}
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 process.title = "Autumn beta";
 
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
 
-const auth = require("./auth/secrets.json");
-const prefix = require("./auth/conf.json");
+const auth = require('./auth/secrets.json'); // token & IDs
+const prefix = require('./auth/conf.json'); // default prefix
 
+if (prefix.length === 0) {
+    console.log('Enter a prefix in the conf.json');
+} // Make sure a default prefix is entered in conf.json as it is blank for you
+
+// Testing Intents with Single Intent
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.login(auth.token)
+// Testing with Discord.js Sample Event Handler
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.login(auth.token) // Make sure to fill in the secrets.json with your token and IDs
+// Never share your token with anyone.
 .catch(console.error);
